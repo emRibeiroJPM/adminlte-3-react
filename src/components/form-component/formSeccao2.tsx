0f365cecf8e7da456821c3a4b7598005f9675142
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2023-12-22 12:30:56 +0000

    : no updates

--- src/components/form-component/formSeccao2.tsx
+++ src/components/form-component/formSeccao2.tsx
@@ -1,36 +1,39 @@
 import {Button,Cascader,Form,Input,InputNumber,Radio,Select,} from 'antd';
 import EscolherData from './date-picker';
-import CodigoQR from '../codigoQR/qrCode-component';
 import {useState } from 'react';
 import ModalComponent from '../modal/ModalComponent';
 import { DatePicker } from 'antd';
 import { DatePickerProps, RangePickerProps } from 'antd/es/date-picker';
-
+import PDFcomponent from '../pdfMaster/PDFcomponent';
 interface Iencomenda {
-  tipoEmbalagem :"",
-  operador:"",
-  referencia:"",
-  quantidadePaletes:"",
-  cliente:"",
-  data: string | [string, string] | undefined
-  produtosPalete:"",
-  lote:""
+  tipoEmbalagem :string,
+  operador:string,
+  referencia:string,
+  quantidadePaletes:number,
+  cliente:string | [string,string],
+  data: string | [string, string] | any
+  produtosPalete:string | number,
+  lote:number
 }
 
 const referênciasFormulário = [
   {
+    id:0,
     label: "Referência Receita A",
     referencia:"40020010",
   },
   {
+    id:1,
     label: "Referência Receita B",
     referencia:"40020020",
   },
   {
+    id:2,
     label: "Referência Receita C",
     referencia:"40020030",
   },
   {
+    id:3,
     label: "Referência Receita D",
     referencia:"40020040",
   }
@@ -74,7 +77,7 @@ const clientesFormulário = [
 const FormularioSeccaoDois = () => {
 
   let prevEncomenda = {Encomenda:{}}
-  const [encomenda,setEncomenda] = useState<Iencomenda>({tipoEmbalagem :"",operador:"",referencia:"",quantidadePaletes:"",cliente:"",data:"",produtosPalete:"",lote:""})
+  const [encomenda,setEncomenda] = useState<Iencomenda>({tipoEmbalagem :"",operador:"",referencia:"",quantidadePaletes:0,cliente:"",data:"",produtosPalete:"",lote:0})
   const [form] = Form.useForm();
   const [modalAberto, setModalAberto] = useState(false)
 
@@ -85,7 +88,7 @@ const FormularioSeccaoDois = () => {
   const noFinalDoFormulario = async () =>{
     prevEncomenda = await form.getFieldsValue(true)
     await setEncomenda((corpoObjeto)=>({...corpoObjeto,...prevEncomenda.Encomenda}))
-    console.log("A data definida pela variavel dataHora é",dataHora)
+    //console.log("A data definida pela variavel dataHora é",dataHora)
     handleAbrirModal();
     //console.log(encomenda)
     //console.log(prevEncomenda);
@@ -122,7 +125,8 @@ const FormularioSeccaoDois = () => {
   /////////////////////////////////////////////////////
   //Introduzir acima as propriedades e funcoes do Date Picker
   /////////////////////////////////////////////////////
-    return (<><Form
+    return (<><div>
+      <Form
           form={form}
           name='FormularioSec2'
           onFinish={onFinish}
@@ -131,7 +135,7 @@ const FormularioSeccaoDois = () => {
           layout="horizontal"
           size={"middle"}
           autoComplete='true'
-        >
+          >
           <Form.Item label="Tipo de Embalagem" name={["Encomenda","tipoEmbalagem"]} rules={[{required:true,message:"Indique qual o tipo de embalagem"}]}>
             <Radio.Group>
               <Radio.Button value="Tipo A">Tipo A</Radio.Button>
@@ -145,8 +149,8 @@ const FormularioSeccaoDois = () => {
           <Form.Item label="Referência" name={["Encomenda","referencia"]} rules={[{required:true,message:"Indique qual a referência da receita"}]}>
             <Select>
               {referênciasFormulário.map((item)=>{
-                return(<Select.Option value={item.referencia}>{item.label}</Select.Option>)
-                })}
+                return(<Select.Option key={item.id} value={item.referencia}>{item.label}</Select.Option>)
+              })}
             </Select>
           </Form.Item>
           <Form.Item label="Nr Total de Paletes" name={["Encomenda","quantidadePaletes"]} rules={[{required:true,message:"Indique qual o numero de paletes"}]}>
@@ -165,13 +169,14 @@ const FormularioSeccaoDois = () => {
             <InputNumber />
           </Form.Item>
           <Form.Item label="Submeter">
-            <Button htmlType="submit">Submeter</Button>
+            <Button type='primary' htmlType="submit">Submeter</Button>
           </Form.Item>
         </Form>
         <div style={{marginLeft:"15%"}}>
-          <CodigoQR informacaoAInserir={encomenda}/>
+          <PDFcomponent {...encomenda}/>
         </div>
         <ModalComponent abrirModal={modalAberto} embalagem={encomenda}/>
+      </div>
     </>);
     };
 
---
+++ src/components/pdfMaster/PDFcomponent.tsx
@@ -0,0 +1,122 @@
+/*
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;900&display=swap" rel="stylesheet"></link>
+Font.register({ family: 'Roboto', src:'https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;900&display=swap'});
+*/
+import { Text, View,StyleSheet, Link } from '@react-pdf/renderer';
+import { PDFDownloadLink, Document, Page,Image } from '@react-pdf/renderer';
+import CodigoQR from '../codigoQR/qrCode-component';
+import { Button } from 'antd';
+import html2canvas from "html2canvas";
+//TRANSFORMAR CODIGO QR EM SVG PARA COLOCAR NON DOCUMENTO PDF
+//https://dev.to/saranshk/how-to-convert-a-react-component-to-an-image-2jon
+
+interface infoPDF{
+    tipoEmbalagem: string,
+    operador: string,
+    referencia:string,
+    quantidadePaletes:number,
+    cliente:[string,string] | string,
+    data:[string,string] | string | any,
+    produtosPalete:number | string,
+    lote:number
+}
+
+const styles = StyleSheet.create({
+  page: {
+    display:'flex',
+    flexDirection: 'column',
+    backgroundColor: '#fff',
+    margin:'10px'
+  },
+  Cabecalho: {
+    maxHeight: '75%',
+    maxWidth: '75%',
+    transformOriginX:'-50%',
+    justifyContent:'space-around'
+  },
+  dadosFormulario: {
+    fontFamily: 'Courier',
+    marginBottom:'25px',
+    marginLeft:'25px'
+  },
+  rodape:{
+    display:'flex',
+    flexDirection:'column'    
+  },
+  link:{
+    fontFamily: 'Courier',
+    textDecoration:'underline',
+    marginLeft:'25px',
+    alignItems:'flex-end'
+  }
+
+
+});
+
+const handleConversaoQRImagem = async (element:any,imageFileName:any) =>{
+  const canvas = await html2canvas(element);
+  const image = canvas.toDataURL('image/png',1.0);
+  console.log(image)
+  return(image);
+}
+
+const pdfDocument = ( texto : infoPDF) =>{
+    //console.log("dentro do pdf este e o texto not string",texto)
+    return(
+    <Document>
+        <Page size="A4" style={styles.page}>
+            <View fixed>
+                <Image 
+                source={"./img/egiquimica.png"}
+                style={styles.Cabecalho}/>
+            </View>
+        <View style={styles.dadosFormulario}>
+            <Text>{`Tipo de embalagem: ${texto.tipoEmbalagem}`}</Text>
+            <Text>{`Operador: ${texto.operador}`}</Text>
+            <Text>{`Referência: ${texto.referencia}`}</Text>
+            <Text>{`Quantidade de Paletes: ${texto.quantidadePaletes}`} paletes</Text>
+            <Text>{`Cliente: ${texto.cliente[0]} ${texto.cliente[1]}`}</Text>
+            <Text>{`Data Início: ${texto.data[0]}`}</Text>
+            <Text>{`Data Fim: ${texto.data[1]}`}</Text>
+            <Text>{`Qtd Produtos por palete: ${texto.produtosPalete}`}</Text>
+            <Text>{`Lote: ${texto.lote}`}</Text>
+        </View>
+        <View style={styles.rodape}>
+            <Text><Link style={styles.link} src='https://www.egiquimica.com/'>Visite a nossa pagina</Link></Text>
+        </View>
+        </Page>
+    </Document>)
+}
+
+function PDFcomponent( props : infoPDF) {
+    const options:any = {
+        weekday: "long",
+        year: "numeric",
+        month: "numeric",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric"
+      }; 
+    const dataAtual = new Date().toLocaleDateString('pt-PT',options);
+  
+    return (
+    <div>
+        <div id="idCodigoQR">
+            <CodigoQR {...props}/>
+        </div>
+        <div>
+            <Button type='primary'>
+                <PDFDownloadLink document={pdfDocument(props)} fileName={`Form-${dataAtual}.pdf`}>
+                {({ blob, url, loading, error }) =>
+                    loading ? 'A carregar...' : 'Transferir PDF'
+                }
+                </PDFDownloadLink>
+            </Button>
+        </div>
+  </div>
+  )
+}
+
+export default PDFcomponent
