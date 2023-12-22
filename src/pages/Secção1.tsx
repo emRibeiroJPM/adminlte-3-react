32a914c7b72eb51da20731299584bf90069f054a
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2023-12-22 12:24:49 +0000

    feat: consome dados provenientes da baseDeDados info dos Tanques

--- src/pages/Secção1.tsx
+++ src/pages/Secção1.tsx
@@ -6,22 +6,17 @@ import {SwapLeftOutlined,SwapRightOutlined} from '@ant-design/icons'
 import { useRef } from 'react';
 import LineChartV2 from '@app/components/line-chart/LineChartV2';
 import { useState,useEffect } from 'react';
-interface dataTanqueType {
-  "label": string | undefined,
-  "value": {
-      "Tanque": string | any,
-      "Nivel": number | any,
-      "MatériaPrima": string | any
-  }
+
+interface dbDataTanqueType {
+  "Tanque": string | any,
+  "Nivel": number | any,
+  "MateriaPrima": string | any
 }
 
-const defaultTanqueVal:dataTanqueType = {
-  "label": "",
-  "value": {
-      "Tanque": "Tanque ?",
-      "Nivel": 0.24,
-      "MatériaPrima": "Materia Prima X"
-  }
+const defaultTanqueVal:dbDataTanqueType = {
+  "Tanque": "Tanque ?",
+  "Nivel": 0.24,
+  "MateriaPrima": "Materia Prima X"
 }
 
 const Secção1 = () => {
@@ -36,34 +31,38 @@ const Secção1 = () => {
     }
   };
   
-  const [tanque1,setTanque1] = useState<dataTanqueType>(defaultTanqueVal)
-  const [tanque2,setTanque2] = useState<dataTanqueType>(defaultTanqueVal)
-  const [tanque3,setTanque3] = useState<dataTanqueType>(defaultTanqueVal)
-  const [tanque4,setTanque4] = useState<dataTanqueType>(defaultTanqueVal)
-  const [tanque5,setTanque5] = useState<dataTanqueType>(defaultTanqueVal)
-  const [tanque6,setTanque6] = useState<dataTanqueType>(defaultTanqueVal)
+  const [tanque1,setTanque1] = useState<dbDataTanqueType>(defaultTanqueVal)
+  const [tanque2,setTanque2] = useState<dbDataTanqueType>(defaultTanqueVal)
+  const [tanque3,setTanque3] = useState<dbDataTanqueType>(defaultTanqueVal)
+  const [tanque4,setTanque4] = useState<dbDataTanqueType>(defaultTanqueVal)
+  const [tanque5,setTanque5] = useState<dbDataTanqueType>(defaultTanqueVal)
+  const [tanque6,setTanque6] = useState<dbDataTanqueType>(defaultTanqueVal)
 
-
-
+/*
+  FUNCAO DEPRECADA
   const recebeInfoTodosTanques = async () => {
     const resposta = await fetch('http://localhost:3000/tanques/all')
     const json:any = await resposta.json()
-    console.log("a resposta todos é",json)
+    //console.log("a resposta todos é",json)
+    return json
+  }
+*/
+  const consomeDadosBD = async ()=>{
+    const resposta = await fetch('http://localhost:3000/tanques')
+    const json:any = await resposta.json()
+    console.log("a resposta da Base de Dados é",json.tanques)
     return json
   }
 
   useEffect(() => {
-    recebeInfoTodosTanques().then((result:any)=>{
-      let objetos = result
-      //console.log("isto vem dos objectos",objetos)
-      //console.log("primeiro objeto",objetos[0])
-      setTanque1(objetos[0])
-      setTanque2(objetos[1])
-      setTanque3(objetos[2])
-      setTanque4(objetos[3])
-      setTanque5(objetos[4])
-      setTanque6(objetos[5])
-      //console.log("isto e o tanque1",tanque1)
+    consomeDadosBD().then((res)=>{
+      let obj = res.tanques;
+      setTanque1(obj[0])
+      setTanque2(obj[1])
+      setTanque3(obj[2])
+      setTanque4(obj[3])
+      setTanque5(obj[4])
+      setTanque6(obj[5])
     })
   }, []);
   
@@ -83,22 +82,22 @@ const Secção1 = () => {
                 justifyContent:'flex-start',
                 overflow:'hidden'}}>
                 <div>
-                  <TankLevel percent={tanque1.value.Nivel} title={tanque1.value.Tanque} materiaPrima={tanque1.value.MatériaPrima}/> 
+                  <TankLevel percent={tanque1.Nivel} title={tanque1.Tanque} materiaPrima={tanque1.MateriaPrima}/> 
                 </div>
                 <div>
-                  <TankLevel percent={tanque2.value.Nivel} title={tanque2.value.Tanque} materiaPrima={tanque2.value.MatériaPrima}/>
+                  <TankLevel percent={tanque2.Nivel} title={tanque2.Tanque} materiaPrima={tanque2.MateriaPrima}/> 
                 </div>
                 <div>
-                  <TankLevel percent={tanque3.value.Nivel} title={tanque3.value.Tanque} materiaPrima={tanque3.value.MatériaPrima}/>
+                  <TankLevel percent={tanque3.Nivel} title={tanque3.Tanque} materiaPrima={tanque3.MateriaPrima}/> 
                 </div>
                 <div>
-                  <TankLevel percent={tanque4.value.Nivel} title={tanque4.value.Tanque} materiaPrima={tanque4.value.MatériaPrima}/>
+                  <TankLevel percent={tanque4.Nivel} title={tanque4.Tanque} materiaPrima={tanque4.MateriaPrima}/> 
                 </div>
                 <div>
-                  <TankLevel percent={tanque5.value.Nivel} title={tanque5.value.Tanque} materiaPrima={tanque5.value.MatériaPrima}/>
+                  <TankLevel percent={tanque5.Nivel} title={tanque5.Tanque} materiaPrima={tanque5.MateriaPrima}/> 
                 </div>
                 <div>
-                  <TankLevel percent={tanque6.value.Nivel} title={tanque6.value.Tanque} materiaPrima={tanque6.value.MatériaPrima}/>
+                  <TankLevel percent={tanque6.Nivel} title={tanque6.Tanque} materiaPrima={tanque6.MateriaPrima}/> 
                 </div>
               </div>
               <div className='buttonsContainer' style={{display:'flex', justifyContent:'center',gap:'15px'}}>
