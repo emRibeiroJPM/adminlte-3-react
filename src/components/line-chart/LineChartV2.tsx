317fb21facba6e93f93f93ffb81926ceb12f389f
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2023-12-29 09:05:07 +0000

    fix: added any type to config in LineChartV2 to be able to deploy to prod(vercel)

--- src/components/line-chart/LineChartV2.tsx
+++ src/components/line-chart/LineChartV2.tsx
@@ -2,7 +2,7 @@ import { Line } from '@ant-design/plots';
 import React from 'react'
 
 function LineChartV2() {
-    const config = {
+    const config : any  = {
         data: {
           type: 'fetch',
           value: 'https://gw.alipayobjects.com/os/antvdemo/assets/data/blockchain.json',
