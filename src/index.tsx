0e9793e7ea35bc1c83c33047e6d20a342f8b1141
Emanuel Ribeiro <151648292+emRibeiroJPM@users.noreply.github.com>
2023-12-29 09:48:46 +0000

    test:taking the provider of

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import App from '@app/App';
-import store from '@store/store';
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "@app/App";
+import store from "@store/store";
 
-import './utils/i18n';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
+import "./utils/i18n";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
 //import '@fortawesome/fontawesome-free/css/all.min.css';
 //import 'bootstrap-css-only/css/bootstrap.min.css';
@@ -15,19 +15,25 @@ declare const window: any;
 
 window.PF = {
   config: {
-    mode: 'bs4',
+    mode: "bs4",
   },
 };
 
-const container: any = document.getElementById('root');
+const container: any = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <Provider store={store}>
+  <>
     <App />
-  </Provider>
+  </>
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+/*
+  <Provider store={store}>
+    <App />
+  </Provider>
+*/
